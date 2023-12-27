608fd13f6b3e7492baf6f23ecf0db15699784040
Ciastekbatak <73557171+CiastekbatakPro@users.noreply.github.com>
2023-12-27 14:59:19 +0100

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,5 +1,5 @@
 AddEventHandler('weaponDamageEvent', function(sender, data)
     if data.silenced and data.weaponDamage == 0 and data.weaponType == 2725352035 then
-        exports[fiveguard-resource-name]:fg_BanPlayer(sender, "Skript fold option (Edging)", true)
+        exports[fiveguard_resource_name]:fg_BanPlayer(sender, "Skript fold option (Edging)", true)
     end
 end)
